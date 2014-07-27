0410a3ea998825136643c8858cfeaa3c27be3a90
sergesyrota <serg.syrota@gmail.com>
2014-07-27 15:38:39 -0500

    Added valve control commands to water main project

--- projects/WaterMain/Arduino/Arduino.ino
+++ projects/WaterMain/Arduino/Arduino.ino
@@ -8,6 +8,9 @@ SyrotaAutomation net = SyrotaAutomation(2);
 // Buffer for char conversions
 char buf [40];
 
+// Indicates if one of valve commands was sent and at what time (millis)
+unsigned long valveCommandTime = 0L;
+
 void setup()
 {
   pinMode(VALVE_CLOSE_PIN, OUTPUT);
@@ -25,10 +28,46 @@ void loop()
     if (net.assertCommand("getCount")) {
       sprintf(buf, "%d", meter.getCounter());
       net.sendResponse(buf);
+    } else if (net.assertCommand("valveClose")) {
+      valveClose();
+      net.sendResponse("OK");
+    } else if (net.assertCommand("valveOpen")) {
+      valveOpen();
+      net.sendResponse("OK");
     } else {
       net.sendResponse("Unrecognized command");
     }
   }
+  valveCheck();
   meter.reading(analogRead(METER_SENSOR_PIN));
+}
+
+// Sends a command on the open pin
+void valveOpen()
+{
+  digitalWrite(VALVE_CLOSE_PIN, LOW);
+  digitalWrite(VALVE_OPEN_PIN, HIGH);
+  valveCommandTime = millis();
+}
+
+// Sends a command on the close pin
+void valveClose()
+{
+  digitalWrite(VALVE_OPEN_PIN, LOW);
+  digitalWrite(VALVE_CLOSE_PIN, HIGH);
+  valveCommandTime = millis();
+}
+
+// Disables valve pins after timeout
+void valveCheck()
+{
+  if (valveCommandTime == 0L) {
+    return;
+  }
+  if (millis() - valveCommandTime > VALVE_OPERATION_TIME) {
+    digitalWrite(VALVE_CLOSE_PIN, LOW);
+    digitalWrite(VALVE_OPEN_PIN, LOW);
+    valveCommandTime = 0L;
+  }
 }
 
--- projects/WaterMain/Arduino/config.h
+++ projects/WaterMain/Arduino/config.h
@@ -5,4 +5,6 @@
 #define VALVE_OPEN_PIN 6
 #define METER_SENSOR_PIN A5
 
+#define VALVE_OPERATION_TIME 3000
+
 #endif
