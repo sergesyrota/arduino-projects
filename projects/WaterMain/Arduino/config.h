d802480354852a8ec3dfcbb0c7c6eab0a852237a
sergesyrota <serg.syrota@gmail.com>
2014-11-30 00:34:20 -0600

    Updated water main config, as valve connections got swapped

--- projects/WaterMain/Arduino/config.h
+++ projects/WaterMain/Arduino/config.h
@@ -1,12 +1,12 @@
 #ifndef WaterMeterConfig_h
 #define WaterMeterConfig_h
 
-#define VALVE_CLOSE_PIN 5
-#define VALVE_OPEN_PIN 6
+#define VALVE_CLOSE_PIN 6
+#define VALVE_OPEN_PIN 5
 #define METER_SENSOR_PIN A5
 #define TEMP_SENSOR_PIN 9
 
-#define VALVE_OPERATION_TIME 3000
+#define VALVE_OPERATION_TIME 4000
 #define TEMP_READ_INTERVAL 60000L
 
 struct TempSensorData {
